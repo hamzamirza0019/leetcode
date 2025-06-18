7771b9898c81081aa747479838ac255d6e3916bd
hamzamirza0019 <mirza.king0019@gmail.com>
2025-06-18 21:50:53 +0530

    Time: 1 ms (80.35%) | Memory: 14.9 MB (37.37%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int, int> hm;
-        vector<int> res;
+        // vector<int> res;
         for(int i=0; i<nums.size(); ++i){
             if(hm.find(target-nums[i])!= hm.end()){
-                res.push_back(hm[target-nums[i]]);
-                res.push_back(i);
-                break;
+                // res.push_back(hm[target-nums[i]]);
+                // res.push_back(i);
+                return {hm[target - nums[i]], i};
             }
 
             else{
                 hm[nums[i]]=i;
             } 
         }
-        return res;
+        return {};
     }
 };
