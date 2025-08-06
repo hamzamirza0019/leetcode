8b38b4543c37bd89711e6679b17909bc023cff4e
hamzamirza0019 <mirza.king0019@gmail.com>
2025-08-06 10:33:53 +0530

    Time: 23 ms (5.52%) | Memory: 61 MB (6.16%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int r=0;
+        for(int i=0; i<weights.size(); ++i){
+            r+= weights[i];
+        }
+        int l=0, minCapacity = 0;
+        while(l<=r){
+            int mid = (l+r)/2;
+            if(check(weights, mid, days)){
+                minCapacity = mid;
+                r= mid-1;
+            }
+            else l = mid+1;
+        }
+
+        return minCapacity;
+
+
+
+    }
+
+    bool check(vector<int> arr, int m , int d){
+        int sum=0, count=1;
+        for(int i=0; i<arr.size(); ++i){
+            if (arr[i] > m) return false;
+            
+            if(sum + arr[i] > m){
+                count++;
+                sum=0;
+            }
+            sum+= arr[i];
+        }
+        return count<= d;
+    }
+
+
+
+};
