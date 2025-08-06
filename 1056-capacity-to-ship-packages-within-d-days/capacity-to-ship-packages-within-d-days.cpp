7ede4e425c267410b878ccbdc319b39ff3ff59a3
hamzamirza0019 <mirza.king0019@gmail.com>
2025-08-06 10:39:42 +0530

    Time: 7 ms (91.94%) | Memory: 35 MB (37.08%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,40 +1,34 @@
 class Solution {
 public:
     int shipWithinDays(vector<int>& weights, int days) {
-        int r=0;
+        int l = 0;
+        int r = 0;
         for(int i=0; i<weights.size(); ++i){
-            r+= weights[i];
+            r+=weights[i];
         }
-        int l = *max_element(weights.begin(), weights.end()), minCapacity = 0;
-        while(l<=r){
-            int mid = (l+r)/2;
+
+        while(l <= r){
+            int mid = l + (r - l) / 2;
             if(check(weights, mid, days)){
-                minCapacity = mid;
-                r= mid-1;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
-            else l = mid+1;
         }
 
-        return minCapacity;
-
-
-
+        return l;
     }
 
-    bool check(vector<int> arr, int m , int d){
-        int sum=0, count=1;
-        for(int i=0; i<arr.size(); ++i){
-            if (arr[i] > m) return false;
-            
-            if(sum + arr[i] > m){
+    bool check(const vector<int>& arr, int m , int d){
+        int sum = 0, count = 1;
+        for(int w : arr){
+            if(w > m) return false;
+            if(sum + w > m){
                 count++;
-                sum=0;
+                sum = 0;
             }
-            sum+= arr[i];
+            sum += w;
         }
-        return count<= d;
+        return count <= d;
     }
-
-
-
 };
