522652ebcf4938e4cf953c05c58544bb6e23dd5e
hamzamirza0019 <mirza.king0019@gmail.com>
2025-09-24 01:20:39 +0530

    Time: 3 ms (89.58%) | Memory: 44.4 MB (26.45%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.java
+++ 496-next-greater-element-i/next-greater-element-i.java
@@ -2,32 +2,26 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         HashMap<Integer, Integer> sm = new HashMap<>();
         Stack<Integer> st = new Stack<>();
-        ArrayList<Integer> ans = new ArrayList<>();
         
-        for(int i=nums2.length-1; i>=0; --i){
-            while(st.size()>0 && st.peek()<= nums2[i]){
+        for (int i = nums2.length - 1; i >= 0; i--) {
+            while (!st.isEmpty() && st.peek() <= nums2[i]) {
                 st.pop();
             }
-            if(st.size() > 0 ){
-                ans.add(st.peek());
+            
+            if (st.isEmpty()) {
+                sm.put(nums2[i], -1);
+            } else {
+                sm.put(nums2[i], st.peek());
             }
-            else{
-                ans.add(-1);
-            }
+            
             st.push(nums2[i]);
         }
         
-        
-        Collections.reverse(ans);
-        for(int i=0; i<nums2.length; ++i){
-            sm.put(nums2[i], ans.get(i));
-        }
         int[] res = new int[nums1.length];
-        for(int i=0 ; i<nums1.length; ++i){
+        for (int i = 0; i < nums1.length; i++) {
             res[i] = sm.get(nums1[i]);
         }
-
+        
         return res;
-
     }
 }
