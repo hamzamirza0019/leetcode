f7afb1482e6d3f5be11016a581bdac87b4b258d9
hamzamirza0019 <mirza.king0019@gmail.com>
2026-01-15 10:18:12 +0530

    Time: 1 ms (64.41%) | Memory: 14.3 MB (40.86%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> res;
+    // unorderes_map<int, int> um;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int> current;
+        sort(candidates.begin(), candidates.end());
         uniqueCombinations( 0, target, current, candidates);
         return res;
     }
 
-    void uniqueCombinations(int start, int remaining , vector<int> current,     vector<int>& candidates){
+    void uniqueCombinations(int start, int remaining , vector<int> &current,     vector<int>& candidates){
 
-        if(remaining< 0) return;
+        
         if(remaining ==0){
             res.push_back(current);
             return;
         }
 
 
+
         for(int i= start; i<candidates.size(); i++){
+            if(candidates[i] > remaining) return; 
             current.push_back(candidates[i]);
             uniqueCombinations(i , remaining - candidates[i], current, 
             candidates);
